7fa33e363532cbadbbae746605427cc65338be19
Vincent <v.groen@student.tudelft.nl>
2022-01-07 16:29:44 +0100

    Progress on the quantum state manager
    This commit integrates the code that Leonardo wrote and furthers the
    development on the quantum state manager.

--- src/quantum_state.py
+++ src/quantum_state.py
@@ -1,8 +1,11 @@
 import os
 
+import numpy as np
+
 from projectq import MainEngine
 from projectq.backends import ResourceCounter
-from projectq.ops import H, All
+from projectq.meta import Dagger
+from projectq.ops import H, All, Ry, TGate, CNOT, SGate, Swap, Measure
 from projectq.setups import restrictedgateset
 
 from quantuminspire.api import QuantumInspireAPI
@@ -19,15 +22,22 @@ class QuantumState:
     'Quantum state manager'
 
     def __init__(self, size=3):
-        """Create a new QuantumState and perform the setup."""
+        """ Create a new QuantumState and perform the setup.
+        
+        Args:
+            size (int): The width and height of the board
+        """
         self.size = size
+        self.command_queue = []
+        self.initial_states = [0.5 for _ in range(size ** 2)] # probabilities to be in the |1> state
         self.setup()
     
 
     def __initialise_qubits(self):
         """Allocate the qubits and place them in the |+> state."""
         self.qubits = self.engine.allocate_qureg(self.size ** 2)
-        All(H) | self.qubits
+        for i in range(len(self.qubits)):
+            Ry(np.pi * self.initial_states[i]) | self.qubits[i]
 
     
     def setup(self):
@@ -52,5 +62,135 @@ class QuantumState:
 
     
     def get_index(self, position):
-        """Convert a 2D (x, y) coordinate into a 1D array index."""
-        return max(0, min(self.size ** 2 - 1, position[1] * self.size + position[0]))
+        """ Convert a 2D (x, y) coordinate into a 1D array index.
+        
+        Args:
+            position (int, int): The x and y coordinates in the 2D grid
+        """
+        x = max(0, min(self.size - 1, position[0]))
+        y = max(0, min(self.size - 1, position[1]))
+        return y * self.size + x
+
+
+    def __execute(self):
+        self.reset()
+        for command in self.command_queue:
+            if command.id is "move": self.__move(*command.data)
+            elif command.id is "entangle": self.__entangle(*command.data)
+            elif command.id is "swap": self.__swap(*command.data)
+            elif command.id is "measure": self.__measure(*command.data)
+            else: print(f"Unknown command {command.id}")
+        self.engine.flush()
+
+        index = self.qubits[self.command_queue[-1].data[0]]
+
+        mresult = self.engine.get_measurement_result(index) # Returns the result of the measurement of qubit `index`
+        probabilities = self.qi_backend.get_probabilities(self.qubits) # Returns all possible states with their probabilities
+
+        # TODO:
+        #   Filter qubit strings based on result of measurement
+        #   Filter probabilities array => { '10101110': 0.5, '10101110': 0.3, ... }
+        #   Normalise resulting probabilities
+        #   Fill initial_states array
+
+
+        # '0100' ~0.25 => [ 0.5, 1, 0.5, 0.5 ]
+        # '0000' ~0.25
+        # '1100' ~0.25
+        # '1000' ~0.25
+
+        # '0100' ~0.5, '1100' ~0.5
+        # send [0.5, 1, 1, 1] 
+
+        
+
+        self.initial_states = [] 
+
+        self.command_queue = [] # Clear the command queue
+
+        return self.initial_states
+
+
+    def measure(self, q, player_id):
+        self.command_queue.append({
+            "id": "measure",
+            "data": [q, player_id]
+        })
+        self.__execute()
+    
+    def __measure(self, q, player_id):
+        Measure | self.qubits[q]
+
+
+    def move(self, q, player_id):
+        """ Classic move: rotation about the y-axis.
+
+        Args:
+            qubits: The qubits
+            q (int): Which qubit is rotated (value from 0 to 8)
+            player_id (int): Which player did the move (value either 1 or 2)                                    
+        """
+        self.command_queue.append({
+            "id": "move",
+            "data": [ q, player_id ]
+        })
+
+    def __move(self, q, player_id):
+        if player_id == 1:
+            Ry(np.pi/2) | self.qubits[q]
+        else:
+            Ry(-np.pi/2) | self.qubits[q]
+
+
+    def entangle(self, q1, q2):
+        """ Entangle move: entangling two qubits.
+
+        Args:
+            qubits: The qubits
+            q1, q2 (int): The qubits which need to be entangled (value from 0 to 8)          
+            engine: Engine is needed for Dagger(engine)                       
+        """
+        self.command_queue.append({
+            "id": "entangle",
+            "data": [ q1, q2 ]
+        })
+
+    def __entangle(self, q1, q2):
+        CNOT | (self.qubits[q1], self.qubits[q2])
+
+        H | self.qubits[q1]
+        with Dagger(self.engine):
+            TGate() | self.qubits[q2]
+        TGate() | self.qubits[q1]
+        H | self.qubits[q2]
+        H | self.qubits[q1]
+
+        CNOT | (self.qubits[q1], self.qubits[q2])
+
+        H | self.qubits[q1]
+        H | self.qubits[q2]
+        with Dagger(self.engine):
+            TGate() | self.qubits[q1]
+        H | self.qubits[q1]
+
+        CNOT | (self.qubits[q1], self.qubits[q2])
+
+        with Dagger(self.engine):
+            SGate() | self.qubits[q1]
+        SGate() | self.qubits[q2]
+
+
+    def swap(self, q1, q2):
+        """ Swap move: swapping two qubits.
+
+        Args:
+            qubits: The qubits
+            q1, q2 (int): The qubits which need to be swapped (value from 0 to 8)                    
+        """
+        self.command_queue.append({
+            "id": "move",
+            "data": [ q1, q2 ]
+        })
+
+    def __swap(self, q1, q2):
+        Swap | (self.qubits[q1], self.qubits[q2])
