b86cb1610419320efb9c83c63716d83617331c15
Vincent <v.groen@student.tudelft.nl>
2022-01-14 10:09:02 +0100

    Reduce output circuit complexity

--- src/quantum_bot.py
+++ src/quantum_bot.py
@@ -4,9 +4,9 @@ O = 2
 
 # Define the state of the board, the algorithm will try to find a win for X
 board_state = [
-    O, O, X,
-    _, _, _,
-    _, _, X
+    O, _, X,
+    _, _, O,
+    X, _, _
 ]
 board_len = len(board_state)
 
@@ -43,57 +43,25 @@ def multicontrolled_toffoli(inputs, out, ancilla_start):
     return result
 
 
-def multicontrolled_or(inputs, out, ancilla_start):
-    """ Generate a multi-controlled `or` circuit
-    
-    Time complexity: O(n) with n = len(inputs)
-    """
-    if len(inputs) == 1:
-        return f"cnot q[{inputs[0]}], q[{out}]\n"
-    
-    if len(inputs) == 2:
-        return f"x q[{inputs[0]}, {inputs[1]}]\ntoffoli q[{inputs[0]}], q[{inputs[1]}], q[{out}]\nx q[{inputs[0]}, {inputs[1]}, {out}]\n"
-
-    result = ""
-    inputs_str = ', '.join(str(n) for n in inputs)
-
-    result += f"x q[{inputs_str}]\n"
-    toffoli = multicontrolled_toffoli(inputs, out, ancilla_start) # O(n)
-    result += toffoli
-    result += f"x q[{inputs_str}, {out}]\n"
-
-    return result
-
-
 def validation(out, ancilla_start):
     """ Generate the validation sub-circuit
     
     Time complexity: O(4n) with n = board_len
     """
     result = ""
-    ancilla = ancilla_start
+    variable_cells = [i for i, v in enumerate(board_state) if v == _] # O(n)
 
-    toffoli = multicontrolled_toffoli(list(range(board_len)), "{out:n}", ancilla_start) # O(n)
-    or_inputs = []
+    toffoli = multicontrolled_toffoli(variable_cells, out, ancilla_start) # O(n)
 
-    ancilla += board_len - 2
-
-    variable_cells_str = ", ".join([str(i) for i, v in enumerate(board_state) if v == _]) # O(n)
+    variable_cells_str = ", ".join(str(i) for i in variable_cells) # O(n)
     result += f"x q[{variable_cells_str}]\n"
 
-    for index, value in enumerate(board_state): # O(n)
-        if value != _:
-            continue
-
+    for index in variable_cells: # O(n)
         result += f"x q[{index}]\n"
-        result += toffoli.format(out=ancilla)
+        result += toffoli
         result += f"x q[{index}]\n"
 
-        or_inputs.append(ancilla)
-        ancilla += 1
     result += f"x q[{variable_cells_str}]\n"
-
-    result += multicontrolled_or(or_inputs, out, or_inputs[0] - len(or_inputs) + 1) # O(n)
 
     return result
 
@@ -101,11 +69,9 @@ def validation(out, ancilla_start):
 def win_condition(out, ancilla_start):
     """ Generate the win condition sub-circuit
     
-    Time complexity: O(9n) with n = board_len
+    Time complexity: O(8n) with n = board_len
     """
     result = ""
-    ancilla = ancilla_start
-    or_inputs = []
 
     conditions = [
         [0, 1, 2],
@@ -120,11 +86,7 @@ def win_condition(out, ancilla_start):
 
     for cond in conditions: # O(8)
         if board_state[cond[0]] != O and board_state[cond[1]] != O and board_state[cond[2]] != O:
-            result += multicontrolled_toffoli(cond, ancilla, ancilla + 1) # O(n)
-            or_inputs.append(ancilla)
-            ancilla += 1
-
-    result += multicontrolled_or(or_inputs, out, ancilla) # O(n)
+            result += multicontrolled_toffoli(cond, out, ancilla_start) # O(n)
 
     return result
 
@@ -151,7 +113,7 @@ def generate_qasm():
     Time complexity: O(27n)
     """
     valid = validation(board_len + 1, board_len + 2) # O(4n)
-    win = win_condition(board_len + 2, board_len + 3) # O(9n)
+    win = win_condition(board_len + 2, board_len + 3) # O(8n)
 
     valid_and_win = valid + "\n" + win
     grovers_bit = f"\ntoffoli q[{board_len + 2}], q[{board_len + 1}], q[{board_len}]\n\n"
@@ -164,11 +126,12 @@ def generate_qasm():
     qasm += valid_and_win + grovers_bit + reverse_lines(valid_and_win) + "\n" # O(13n)
     qasm += diffuser(board_len, board_len + 1) # O(n)
 
-    # Not measuring is faster
-    # qasm += f"\n.measurement\nmeasure_z q[{', '.join([str(index) for index, value in enumerate(board_state) if value == _])}]"
+    # Not measuring is faster, but then the results will have to be filtered afterwards
+    qasm += f"\n.measurement\nmeasure_z q[{', '.join([str(index) for index, value in enumerate(board_state) if value == _])}]" # O(n)
 
-    # TODO: Calculate the actual amount of qubits used
-    qasm = f"version 1.0\n\nqubits {26}\n\n" + qasm
+    # The circuit will always use 17 qubits
+    # This limit is mostly defined by the multi-controlled toffoli for the diffuser
+    qasm = f"version 1.0\n\nqubits {17}\n\n" + qasm
 
     return qasm
 
